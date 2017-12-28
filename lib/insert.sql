b2cb2274abf608b88201e9e6aa1ffd39102235d1
Michelle Wiraydi <miwiryadi@gmail.com>
2017-12-28 16:56:09 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,8 @@
-describe 'populating the bears table' do
-  before do
-    @db = SQLite3::Database.new(':memory:')
-    @sql_runner = SQLRunner.new(@db)
-    @sql_runner.execute_create_file
-    @sql_runner.execute_insert_file
-  end
-
-  it 'has 8 bears' do
-    expect(@db.execute("SELECT COUNT(*) FROM bears;").flatten[0]).to eq(8)
-  end
-
-  it 'has an unnamed bear' do
-    expect(@db.execute("SELECT COUNT(*) FROM bears WHERE name IS NULL;").flatten[0]).to eq(1)
-  end
-end
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 10, "M", "black", "intense", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 6, "F", "dark brown", "Nice", 0);
+INSERT INTO bears VALUES (4,"Sargeant Brown", 19, "M", "Green", "Slimy", 1);
+INSERT INTO bears VALUES (5,"Melissa", 13, "F", "dark brown", "goofy", 1);
+INSERT INTO bears VALUES (6,"Grinch", 2, "M", "Black", "Grinchy", 1);
+INSERT INTO bears VALUES (7,"Wendy", 12, "F", "Blue", "naive", 0);
+INSERT INTO bears VALUES (8,null, 20, "M", "black", "aggressive", 0
